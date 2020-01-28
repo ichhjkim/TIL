592216816b45d453e49e2cafd21612f96702d69a
ichhjkim <nillcafri123@gmail.com>
2020-01-28 19:58:09 +0900

    스프링부트 3. 경로패턴 설정

--- 02_spring/MVCProject/src/main/java/org/hdcd/controller/BoardController.java
+++ 02_spring/MVCProject/src/main/java/org/hdcd/controller/BoardController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.hdcd.domain.Board;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -17,37 +18,76 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/boards")
 public class BoardController {
-	private static final Logger logger =
-			LoggerFactory.getLogger(BoardController.class);
+	
+	private static final Logger logger =LoggerFactory.getLogger(BoardController.class);
 	
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public ResponseEntity<List<Board>> list() {
 		logger.info("list");
 		
-		List<Board> BoardList = new ArrayList<>();
+		List<Board> boardList = new ArrayList<>();
 		Board board = new Board();
 		
 		board.setBoardNo(1);
 		board.setTitle("향수");
 		board.setContent("넓은 별 동쪽 끝으로");
 		board.setWriter("hongkd");
-		board.setRedDate(new Date());
+		board.setRegDate(new Date());
+		
+		boardList.add(board);
+		
+		board = new Board();
+		
+		board.setBoardNo(2);
+		board.setTitle("첫 마음");
+		board.setContent("날마다 새로우며 깊어지고 넓어진다");
+		board.setWriter("hongkd");
+		board.setRegDate(new Date());
 		
 		boardList.add(board);
 		
 		ResponseEntity<List<Board>> entity = new ResponseEntity<>(boardList, HttpStatus.OK);
 		return entity;
 	}
+	
 	@RequestMapping(value = "", method = RequestMethod.POST)
-	public ResponseEntity<String> register(@RequestBody Board board()) {
+	public ResponseEntity<String> register(@RequestBody Board board) {
 		logger.info("register");
 		
 		ResponseEntity<String> entity = new ResponseEntity<>("SUCCESS", HttpStatus.OK);
 		return entity;
 	}
+	
 	@RequestMapping(value = "/{boardNo}", method=RequestMethod.GET)
 	public ResponseEntity<Board> read(@PathVariable("boardNo") int boardNo) {
 		logger.info("read");
+		
+		Board board = new Board();
+		board.setBoardNo(1);
+		board.setTitle("향수");
+		board.setContent("넓은 별 동쪽 끝으로");
+		board.setWriter("hongkd");
+		board.setRegDate(new Date());
+		
+		ResponseEntity<Board> entity = new ResponseEntity<>(board, HttpStatus.OK);
+		
+		return entity;
+		
+		
 	}
-
+		@RequestMapping(value = "/{boardNo}", method=RequestMethod.DELETE)
+		public ResponseEntity<String> remove(@PathVariable("boardNo") int boardNo) {
+			logger.info("remove");
+			
+		ResponseEntity<String> entity = new ResponseEntity<>("SUCCESS", HttpStatus.OK);
+		
+		return entity;
+		}
+		@RequestMapping(value = "/{boardNo}", method=RequestMethod.PUT)
+		public ResponseEntity<String> modify(@PathVariable("boardNo") int boardNo, @RequestBody Board board) {
+			logger.info("modify");
+			
+			ResponseEntity<String> entity = new ResponseEntity<>("SUCCESS", HttpStatus.OK);
+			return entity;
+		}
 }
---
+++ 02_spring/makingBoard/src/main/java/com/example/demo/MakingBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakingBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakingBoardApplication.class, args);
+	}
+
+}
