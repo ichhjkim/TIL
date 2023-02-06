567956f933c5d03f665c5efb85e2ef679b779369
ichhjkim <nillcafri123@gmail.com>
2023-02-06 22:51:16 +0900

    [feat] log tracker version3 feature and test
    - FieldLogTrace with 동시성 이슈

---
+++ 01_Spring/01_logTracker/src/main/java/hello/advanced/app/v3/OrderControllerV3.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+    private final OrderServiceV3 orderService;
+    private final LogTrace logTrace;
+
+    @GetMapping("/v3/request")
+    public String request(String itemId) {
+        TraceStatus traceStatus = null;
+        try {
+            traceStatus = logTrace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            logTrace.end(traceStatus);
+            return "ok";
+        } catch (Exception e) {
+            logTrace.exception(traceStatus, e);
+            throw e;
+        }
+    }
+}
